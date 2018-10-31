b28ec910f6c2a4c3248f05dd7a1a0790cb341498
yapjinai <jinai.jyap@gmail.com>
2018-10-31 11:03:57 -0400

    Done.

--- src/BabyHog.js
+++ src/BabyHog.js
@@ -1,3 +1,12 @@
+// BabyHog:
+
+// Has an eye color received from its parent
+// Has a hobby assigned by its parent
+// Has a name given by its parent
+// Has a variable weight, that can be changed via buttons
+
+// Note: While the MasterHog component can change its own eye color via radio buttons, its children can only inherit the eye color of their parent!
+
 import React, { Component } from 'react'
 import { Button } from 'semantic-ui-react'
 import normalBaby from './assets/unadulterated-hoglette.png'
@@ -5,13 +14,22 @@ import SunBaby from './assets/sun-eyes.png'
 import BlueBaby from './assets/blue-eyes.png'
 import GlowingBaby from './assets/glowing-eyes.png'
 
-// is there a way we could associate eye color string values with images? 
+// is there a way we could associate eye color string values with images?
 // perhaps so we could do something along the lines of `eyeColorMapper['blue'] and get back the right image?`
+
+const eyeColorMapper = {
+  blue: BlueBaby,
+  sun: SunBaby,
+  glowing: GlowingBaby
+}
 
 export default class BabyHog extends Component {
 
   constructor(props) {
     super(props)
+    this.state = {
+      weight: 10
+    }
   }
 
   changeWeight = (e) => {
@@ -25,22 +43,28 @@ export default class BabyHog extends Component {
   render() {
     return (
       <li className="hogbabies">
-        <h1>Name</h1>
-        <h3>Weight:</h3>
-        <h3>Hobby:</h3>
-        <h4>Eye Color:</h4>
-          
-        <Button name="+">
+        <h1>Name</h1> {this.props.hog.name}
+        <h3>Weight:</h3> {this.state.weight}
+        <h3>Hobby:</h3> {this.props.hog.hobby}
+        <h4>Eye Color:</h4> {this.props.eyeColor}
+
+        <Button
+          name="+"
+          onClick={this.changeWeight}
+        >
           Increase Weight
         </Button>
-        <Button name="-">
+        <Button
+          name="-"
+          onClick={this.changeWeight}
+        >
           Decrease Weight
         </Button>
 
         <div className="hb-wrap">
-          <img src={normalBaby} style={{height: '200px'}} alt="MasterBlasterJrJr" />
+          <img src={eyeColorMapper[this.props.eyeColor]} style={{height: '200px'}} alt="MasterBlasterJrJr" />
         </div>
-        
+
       </li>
     )
   }
--- src/MasterHog.js
+++ src/MasterHog.js
@@ -1,7 +1,14 @@
+// MasterHog:
+
+// Has an eye color that can change via a radio button on the DOM
+// Renders three BabyHogs, which inherit its eye color
+// Owns the non-variable data associated with each BabyHog. This includes their: name, eye color, and hobby. (Obviously, MasterHog gets to name her babies, and they genetically inherit her eye color. Not obviously, MasterHog also gets to determine their hobbies)
+
+
 import React, { Component } from 'react'
 import Master from './assets/master-hog.png'
 import BabyHog from './BabyHog'
-// import offspring from './db.js'
+import offspring from './db.js'
 
 export default class MasterHog extends Component {
 
@@ -12,10 +19,20 @@ export default class MasterHog extends Component {
     }
   }
 
-
   changeEyeColor = (e) => {
     this.setState({
       eyeColor: e.target.value
+    })
+  }
+
+  generateBabyHogs = () => {
+    return offspring.map((hog) => {
+      return (
+        <BabyHog
+          eyeColor={this.state.eyeColor}
+          hog={hog}
+        />
+      )
     })
   }
 
@@ -30,19 +47,17 @@ export default class MasterHog extends Component {
         Sun<br></br>
         <input type="radio" name="eyeColor" value="glowing" onChange={this.changeEyeColor} />
         Glowing<br></br>
-      
+
         <h2>Name: Master Blaster</h2>
         <h3>Weight: 2.54 Tons</h3>
         <h3>Eye Color: {this.state.eyeColor}</h3>
-        
+
         <div id="masters-domicile">
           <img id="master-blaster" src={Master} alt="" />
         </div>
-        
+
         <ul className="hoglist">
-          <BabyHog />
-          <BabyHog />
-          <BabyHog />
+          {this.generateBabyHogs()}
         </ul>
 
       </div>
